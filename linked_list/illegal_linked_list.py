a0b9e36fc315801ffdfb19d637910f38a8b4c1c4
eck7965 <eck7965@gmail.com>
2020-06-23 23:55:41 +0900

    add text editor

--- linked_list/illegal_linked_list.py
+++ linked_list/illegal_linked_list.py
@@ -5,9 +5,8 @@ nxt = [-1] * MAX
 unused = 1
 
 
-def insert(addr: int, value: int) -> None:
+def insert(addr, value):
     global unused
-
     data[unused] = value
     pre[unused] = addr
     nxt[unused] = nxt[addr]
@@ -20,21 +19,34 @@ def insert(addr: int, value: int) -> None:
     unused += 1
 
 
-def erase(addr: int) -> None:
+def erase(addr):
     nxt[pre[addr]] = nxt[addr]
 
     if nxt[addr] != -1:
         pre[nxt[addr]] = pre[addr]
 
 
-def travel() -> None:
+def travel(end=","):
     cursor = nxt[0]
 
     while cursor != -1:
-        print(f"{data[cursor]}  ", end="  ")
+        print(f"{data[cursor]}", end=end)
         cursor = nxt[cursor]
+
+    print("")
 
 
 if __name__ == '__main__':
     insert(0, 10)
-    travel()
+    travel()  # 10
+    insert(1, 20)
+    travel()  # 10 20
+    insert(2, 30)
+    travel()  # 10 20 30
+
+    erase(1)
+    travel()  # 20 30
+
+    insert(2, 40)
+    travel()  # 20 40 30
+
---
+++ linked_list/text_editor.py
@@ -0,0 +1,68 @@
+MAX = 500000
+_data = [-1] * MAX
+pre = [-1] * MAX
+nxt = [-1] * MAX
+unused = 1
+
+
+def insert(addr, value):
+    global unused
+    _data[unused] = value
+    pre[unused] = addr
+    nxt[unused] = nxt[addr]
+
+    if nxt[addr] != -1:
+        pre[nxt[addr]] = unused
+
+    nxt[addr] = unused
+
+    unused += 1
+
+
+def erase(addr):
+    nxt[pre[addr]] = nxt[addr]
+
+    if nxt[addr] != -1:
+        pre[nxt[addr]] = pre[addr]
+
+
+def travel(end=","):
+    _cursor = nxt[0]
+
+    while _cursor != -1:
+        print(f"{_data[_cursor]}", end=end)
+        _cursor = nxt[_cursor]
+
+    print("")
+
+
+data = input()
+cursor = 0
+
+for i in data:
+    insert(cursor, i)
+    cursor += 1
+
+n = int(input())
+list_len = len(data)
+
+while n > 0:
+    cmd = input()
+    n -= 1
+
+    if cmd[0] == "P":
+        insert(cursor, cmd[2])
+        cursor = nxt[cursor]
+    elif cmd[0] == "L":
+        if pre[cursor] != -1:
+            cursor = pre[cursor]
+    elif cmd[0] == "D":
+        if nxt[cursor] != -1:
+            cursor = nxt[cursor]
+    elif cmd[0] == "B":
+        if cursor == 0:
+            continue
+        erase(cursor)
+        cursor = pre[cursor]
+
+travel(end="")
