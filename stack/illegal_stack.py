715610038f12f3832944e69d2e631ef24df3ec1f
eck7965 <eck7965@gmail.com>
2020-06-26 10:51:41 +0900

    add default stack function

--- stack/illegal_stack.py
+++ stack/illegal_stack.py
@@ -1,4 +1,4 @@
-MAX = 10000
+MAX = 10006
 _data = [0] * MAX
 pos = 0
 
@@ -15,7 +15,7 @@ def push(val):
 def pop():
     global pos
     if pos <= 0:
-        return
+        return -1
 
     pos -= 1
     val = _data[pos]
@@ -23,15 +23,28 @@ def pop():
     return val
 
 
+def size():
+    return pos
+
+
+def top():
+    if pos <= 0:
+        return -1
+    return _data[pos-1]
+
+
 if __name__ == '__main__':
     push(1)  # 1
+    print(top())  # 1
     print(pop())  # 1
 
     push(2)  # 2
     push(3)  # 2 3
+    print(size())  # 2
     print(pop())  # 3
 
     push(4)  # 2 4
+    print(top())  # 4
     print(pop())  # 4
     print(pop())  # 2
-    print(pop())  # None
+    print(pop())  # -1
