cbd743c2b2ca2db01cf3882b66605c9eb6126fe5
eck7965 <eck7965@gmail.com>
2020-07-01 20:38:49 +0900

    add solve baekjoon 10845

--- algorithm_go/src/queue/10845/main.go
+++ algorithm_go/src/queue/10845/main.go
@@ -4,18 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"queue"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	//var que queue.Queue
+	que := queue.Queue()
 
 	r := bufio.NewReader(os.Stdin)
 	w := bufio.NewWriter(os.Stdout)
 
-
 	var n int
 
 	fmt.Fscanln(r, &n)
-	fmt.Fprintln(w, -1)
+
+	for i :=0; i<n; i++{
+		line, _, _ := r.ReadLine()
+		cmd := strings.Split(string(line), " ")
+
+		if cmd[0] == "push" {
+			number, _ := strconv.Atoi(cmd[1])
+			que.Push(number)
+		} else if cmd[0] == "pop" {
+			fmt.Fprintln(w, que.Pop())
+		} else if cmd[0] == "size" {
+			fmt.Fprintln(w, que.Size())
+		} else if cmd[0] == "empty" {
+			fmt.Fprintln(w, que.Empty())
+		} else if cmd[0] == "front" {
+			fmt.Fprintln(w, que.Front())
+		} else if cmd[0] == "back" {
+			fmt.Fprintln(w, que.Back())
+		}
+	}
+
+	w.Flush()
 
 }
