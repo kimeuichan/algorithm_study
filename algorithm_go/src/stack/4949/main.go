befff15a811ef9fb3ef6e9ff216535dd3daed4b5
eck7965 <eck7965@gmail.com>
2020-07-02 23:50:23 +0900

    add baekjoon 4949

--- algorithm_go/src/stack/4949/main.go
+++ algorithm_go/src/stack/4949/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"stack"
+)
 
+func main() {
+	w := bufio.NewWriter(os.Stdout)
+	r := bufio.NewReader(os.Stdin)
+
+	for true {
+		line, _, _ := r.ReadLine()
+
+		realLine := string(line)
+
+		if realLine == "."{
+			break
+		}
+
+		tagMap := map[int32]int32{
+			']': '[',
+			')': '(',
+		}
+
+		s := stack.Stack(10000)
+
+		for _, char := range realLine {
+			if char == '(' || char == '[' {
+				s.Push(int(char))
+			} else if char == ')' ||  char == ']'{
+				closingTag := tagMap[char]
+
+				if s.Top() == int(closingTag) {
+					s.Pop()
+				} else {
+					s.Push(int(char))
+					break
+				}
+			}
+		}
+
+		if s.Empty() == 1 {
+			fmt.Fprintln(w, "yes")
+		} else {
+			fmt.Fprintln(w, "no")
+		}
+	}
+
+	w.Flush()
 }
