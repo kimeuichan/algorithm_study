88b513a4a0357e977548fe94958851dd0620217a
eck7965 <eck7965@gmail.com>
2020-07-02 14:40:04 +0900

    add stack

---
+++ algorithm_go/src/stack/stack.go
@@ -0,0 +1,46 @@
+package stack
+
+const MAX = 10005
+
+type stack struct{
+	items [] int
+	top int
+}
+
+func Stack(itemLen int) *stack {
+	len := itemLen
+
+	if MAX < len{
+		len = MAX
+	}
+
+	return &stack{
+		items: make([]int, len),
+		top: 0,
+	}
+}
+
+func (s *stack) Push (val int) {
+	s.items[s.top] = val
+	s.top++
+}
+
+func (s *stack) Pop () int {
+	if s.Empty() == 0 {
+		return -1
+	}
+
+	s.top--
+	return s.items[s.top]
+}
+
+func (s *stack) Empty () int {
+	if s.top == 0 {
+		return 1
+	}
+	return 0
+}
+
+func (s *stack) Size () int {
+	return s.top
+}
